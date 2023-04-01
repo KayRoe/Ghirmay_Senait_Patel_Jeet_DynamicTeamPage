8ac7c8f1c000a63ffb061c4e1726f2c7587ceb7a
Jeetpatel1507 <54991963+Jeetpatel1507@users.noreply.github.com>
2023-04-01 16:31:21 -0400

    all done

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ function showMember(member) {
     nameElement.innerHTML = nameElement.innerHTML;
     imageElement.src = `./images/${imageName}`;
   }
+
+  function showMember(member) {
+    const activeMember = document.querySelector('.team-member.active');
+    activeMember.classList.remove('active');
+    const newActiveMember = document.querySelector(`#member${member}`);
+    newActiveMember.classList.add('active');
+    updateTeamMember(newActiveMember);
+    
+    const activeButton = document.querySelector('.nav button.active');
+    activeButton.classList.remove('active');
+    const newActiveButton = document.querySelector(`.nav button:nth-child(${member})`);
+    newActiveButton.classList.add('active');
+  }
+
+
+  window.onload = function() {
+    document.querySelector('.nav button:nth-child(1)').classList.add('active');
+}
